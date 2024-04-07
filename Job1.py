6a6f66a274cc71d362fec5030169213405a8c9e6
InnaKirienko <in.kirienko@gmail.com>
2024-04-07 16:46:15 +0300

    ttt

---
+++ Job1.py
@@ -0,0 +1,67 @@
+import os
+import requests
+import json
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
+
+
+def clear_directory(directory):
+    # Очистка вмісту директорії
+    file_list = os.listdir(directory)
+    for file in file_list:
+        os.remove(os.path.join(directory, file))
+
+
+def fetch_sales_data(date, page):
+    # Витягнення даних з API
+    url = 'https://fake-api-vycpfa6oca-uc.a.run.app/sales'
+    params = {'date': date, 'page': page}
+    headers = {'Authorization': AUTH_TOKEN}
+    response = requests.get(url, params=params, headers=headers)
+    return response.json()
+
+
+def save_sales_data_to_file(directory, date, page, data):
+    # Зберігання даних у файл
+    filename = f"sales_{date}_{page}.json"
+    filepath = os.path.join(directory, filename)
+    with open(filepath, 'w') as f:
+        json.dump(data, f)
+
+
+@app.route('/', methods=['POST'])
+def handle_post_request():
+    # Отримання JSON-об'єкту з запиту
+    data = request.json
+
+    # Перевірка, чи існує об'єкт та його властивості
+    if data and 'date' in data and 'raw_dir' in data:
+        date = data['date']
+        raw_dir = data['raw_dir']
+
+        # Створення шляху для зберігання
+        directory = os.path.join(raw_dir, 'sales', date)
+        os.makedirs(directory, exist_ok=True)
+
+        # Очищення вмісту директорії
+        clear_directory(directory)
+
+        page = 1
+        while True:
+            sales_data = fetch_sales_data(date, page)
+            if not sales_data:
+                break
+            save_sales_data_to_file(directory, date, page, sales_data)
+            page += 1
+
+        return jsonify({'message': 'Sales data fetched and saved successfully'}), 200
+    else:
+        return jsonify({'error': 'Invalid JSON format'}), 400
+
+
+if __name__ == '__main__':
+    app.run(port=8081)
