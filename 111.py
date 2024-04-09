7afd7d0f82502297e8eac688cabfc7a25f99ee5a
InnaKirienko <in.kirienko@gmail.com>
2024-04-09 19:32:57 +0300

    ttt

--- 111.py
+++ 111.py
@@ -1,3 +1,4 @@
+# приклад
 import os
 import requests
 
--- Job2.py
+++
@@ -1,9 +0,0 @@
-import os
-
-AUTH_TOKEN = os.getenv('AUTH_TOKEN')
-
-if not AUTH_TOKEN:
-  print("Помилка: Змінна середовища AUTH_TOKEN не встановлена!")
-  exit(1)
-
-# Решта вашого коду, який використовує AUTH_TOKEN
