d9f13969dccf7dde7d127f7f7a551dc3e9cebba0
InnaKirienko <in.kirienko@gmail.com>
2024-04-16 21:55:43 +0300

    commit

--- lesson_02/sales_api.py
+++ lesson_02/sales_api.py
@@ -2,7 +2,7 @@
 
 import requests
 
-def get_sales_data(date: str, page: int, auth_token: str) -> dict:
+def get_sales_data(date: str, page: int, AUTH_TOKEN: str) -> dict:
     #Getting data from the API and uploading it in json format.
     url = 'https://fake-api-vycpfa6oca-uc.a.run.app/sales'
     params = {'date': date, 'page': page}
